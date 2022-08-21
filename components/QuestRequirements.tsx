cc210d8ed898b34608ee48fce43a7898234003be
Jeffrey Tan <jeffreyotan@yahoo.com>
2022-08-22 00:25:32 +0800

    ODY-10004 Bug fixes and asset updates

--- components/QuestRequirements.tsx
+++ components/QuestRequirements.tsx
@@ -13,21 +13,19 @@ const QuestRequirements: NextPage<RequirementsProps> = (requirements) => {
 
     return (
         <>
-            <div className="grid grid-rows-5 h-full p-1">
+            <div className="grid grid-rows-6 h-full p-1">
                 <div className="row-span-1 w-full h-full flex justify-center items-center">
                     <span>REQUIREMENTS</span>
                 </div>
-                <div className="row-span-4 w-full h-full bg-gray-700">
-                    <div className="grid grid-cols-3 w-full">
+                <div className="row-span-5 w-full h-full bg-gray-700 overflow-auto">
                     {requirementsList.map((requirement: Requirement) => (
-                        <>
-                            <div key={index++} className="col-span-2 p-3">{requirement.name}</div>
-                            <div key={index++} className="col-span-1 flex justify-end items-center p-3">
+                        <div key={index++} className="grid grid-cols-3 w-full">
+                            <div className="col-span-2 p-3">{requirement.name}</div>
+                            <div className="col-span-1 flex justify-end items-center p-3">
                                 <meter className="h-3" value={20} min={0} max={100}></meter>
                             </div>
-                        </>
+                        </div>
                     ))}
-                    </div>
                 </div>
             </div>
         </>
